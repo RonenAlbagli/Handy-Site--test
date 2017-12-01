c5da4c6ecf256c0c0ffd72e84939181cafc235ef
RonenAlbagli <AlbagliRonen@gmail.com>
2017-12-01 17:31:03 +0200

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
